7fa1e27408576d98ec6bb4c335db4c7c5f41d4c7
allenandreim <allenandrei8209@gmail.com>
2025-03-13 20:35:51 +0800

    code output(1)

--- 1_1_two_numbers_print_bigger.py
+++ 1_1_two_numbers_print_bigger.py
@@ -1,12 +1,12 @@
-#get two numbers
-#check which is bigger
-#print result
+# get two numbers
+# check which is bigger
+# print result
 
-#get two numbers
+# get two numbers
 num1 = int(input("1st number: "))
 num2 = int(input("2nd number: "))
  
-#check which is bigger
+# check which is bigger
 if num1 > num2 :
     print(num1,"is bigger.")
 elif num2 > num1 :
