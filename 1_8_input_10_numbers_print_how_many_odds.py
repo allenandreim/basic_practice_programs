ddc51d2cf6c3b4029378f7463a8263d8f3263f75
allenandreim <allenandrei8209@gmail.com>
2025-03-14 00:07:29 +0800

    initial_commit_number_inputs

--- 1_8_input_10_numbers_print_how_many_odds.py
+++ 1_8_input_10_numbers_print_how_many_odds.py
@@ -2,4 +2,22 @@
 # check if a number is odd using %2
 # print how many odds
 
+odd_count = 0    #initialize odd count
 
+# get 10 numbers
+def number_input() :
+    num1 = float(input("1st number: "))
+    num2 = float(input("2nd number:"))
+    num3 = float(input("3rd number: "))
+    num4 = float(input("4th number: "))
+    num5 = float(input("5th number: "))
+    num6 = float(input("6th number: "))
+    num7 = float(input("7th number: "))
+    num8 = float(input("8th number: "))
+    num9 = float(input("9th number: "))
+    num10 = float(input("10th number: "))
+
+
+number_input()
+
+    
