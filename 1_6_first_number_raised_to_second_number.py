7a8d74e08c3cfe309294f98a694402892000af09
allenandreim <allenandrei8209@gmail.com>
2025-03-13 21:56:43 +0800

    code_output_1_6

--- 1_6_first_number_raised_to_second_number.py
+++ 1_6_first_number_raised_to_second_number.py
@@ -2,3 +2,12 @@
 # raise num1 to num2
 # print result
 
+# get 2 number
+num1 = float(input("1st number(base): "))
+num2 = float(input("2nd number(exponent): "))
+
+# raise num1 to num2
+outcome = num1 ** num2
+
+# print result
+print(outcome)
