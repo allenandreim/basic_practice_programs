dba8e5f82e33f9dca23b3baccbbaa4f01a1f0e20
allenandreim <allenandrei8209@gmail.com>
2025-03-13 21:11:29 +0800

    code_output_1_4

--- 1_4_two_numbers_print_product.py
+++ 1_4_two_numbers_print_product.py
@@ -1,3 +1,13 @@
 # get 2 numbers
 # multiply 2 numbers
 # print result
+
+# get 2 numbers
+num1 = float(input("1st number: "))
+num2 = float(input("2nd number: "))
+
+# multiply 2 numbers
+product = num1 * num2
+
+# print result
+print(product)
