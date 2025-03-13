81ee0595677b4c98153e32f5995f400463d20af9
allenandreim <allenandrei8209@gmail.com>
2025-03-13 20:59:18 +0800

    code_output_1_3

--- 1_3_two_numbers_print_sum.py
+++ 1_3_two_numbers_print_sum.py
@@ -2,3 +2,12 @@
 # add 2 numbers
 # print result
 
+# get 2 numbers
+num1 = float(input("!st number: "))
+num2 = float(input("2nd number: "))
+
+# add 2 numbers
+sum = num1 + num2
+
+# print result
+print(sum)
