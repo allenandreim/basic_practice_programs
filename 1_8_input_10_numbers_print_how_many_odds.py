ff60188ab03d2a10bc34eaa9395fc2b6e6dcba44
allenandreim <allenandrei8209@gmail.com>
2025-03-14 00:55:52 +0800

    final_commit_code_output_1_8

--- 1_8_input_10_numbers_print_how_many_odds.py
+++ 1_8_input_10_numbers_print_how_many_odds.py
@@ -2,22 +2,43 @@
 # check if a number is odd using %2
 # print how many odds
 
-odd_count = 0    #initialize odd count
+# Initialize odd count
+odd_count = 0
 
-# get 10 numbers
-def number_input() :
-    num1 = float(input("1st number: "))
-    num2 = float(input("2nd number:"))
-    num3 = float(input("3rd number: "))
-    num4 = float(input("4th number: "))
-    num5 = float(input("5th number: "))
-    num6 = float(input("6th number: "))
-    num7 = float(input("7th number: "))
-    num8 = float(input("8th number: "))
-    num9 = float(input("9th number: "))
-    num10 = float(input("10th number: "))
+# Get 10 numbers
+num1 = int(input("1st number: "))
+num2 = int(input("2nd number: "))
+num3 = int(input("3rd number: "))
+num4 = int(input("4th number: "))
+num5 = int(input("5th number: "))
+num6 = int(input("6th number: "))
+num7 = int(input("7th number: "))
+num8 = int(input("8th number: "))
+num9 = int(input("9th number: "))
+num10 = int(input("10th number: "))   
 
+#check if a number is odd and count
+if num1 % 2 != 0:
+    odd_count += 1
+if num2 % 2 != 0:
+    odd_count += 1
+if num3 % 2 != 0:
+   odd_count += 1
+if num4 % 2 != 0:
+    odd_count += 1
+if num5 % 2 != 0:
+    odd_count += 1
+if num6 % 2 != 0:
+    odd_count += 1
+if num7 % 2 != 0:
+    odd_count += 1
+if num8 % 2 != 0:
+    odd_count += 1
+if num9 % 2 != 0:
+    odd_count += 1
+if num10 % 2 != 0:
+    odd_count += 1
 
-number_input()
-
+# print result
+print("There are", odd_count, "odd numbers.")
     
