a91292d80d59c307e0e814ed9ffd712c71f7884a
allenandreim <allenandrei8209@gmail.com>
2025-03-13 21:26:57 +0800

    code_output_1_5

--- 1_5_two_numbers_print_quotient.py
+++ 1_5_two_numbers_print_quotient.py
@@ -1,5 +1,17 @@
 # get 2 numbers
-# divide 1st to 2nd
+# divide num1 to num2
 # restrict 0 in divisor 
 # print result 
 
+# get 2 numbers
+num1 = float(input("1st number(dividend): "))
+num2 = float(input("2nd number(divisor): "))
+
+# divide num1 to num2
+quotient = num1 / num2
+
+# restrict 0 in divisor 
+if num2 != 0 :
+    print(quotient)
+else :
+    print("undefined")
