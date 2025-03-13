132333eadc26520fde28311283816087cdf1644f
allenandreim <allenandrei8209@gmail.com>
2025-03-13 22:07:35 +0800

    code_output_1_7

--- 1_7_input_10_numbers_print_sum.py
+++ 1_7_input_10_numbers_print_sum.py
@@ -2,3 +2,20 @@
 # add all numbers
 # print result
 
+# get 10 numbers
+num1 = float(input("1st number: "))
+num2 = float(input("2nd number:"))
+num3 = float(input("3rd number: "))
+num4 = float(input("4th number: "))
+num5 = float(input("5th number"))
+num6 = float(input("6th number: "))
+num7 = float(input("7th number: "))
+num8 = float(input("8th number: "))
+num9 = float(input("9th number: "))
+num10 = float(input("10th number: "))
+
+# add all numbers
+sum = num1 + num2 + num3 + num4+ num5 + num6 + num7 + num8 + num9 + num10
+
+# print result
+print("the sum of all number is:", sum)
