df8b60cffffd6fdf6aeaa3b72c61512475e9d5b8
allenandreim <allenandrei8209@gmail.com>
2025-03-14 15:45:57 +0800

    code_2_10_progress_getting_numbers_between

--- 2_10_print_numbers_between_2_numbers.py
+++ 2_10_print_numbers_between_2_numbers.py
@@ -4,5 +4,10 @@
 
 # get 2 numbers
 print("input two numbers (whole numbers only)")
-num1 = float(input("1st number: "))
-num2 = float(input("2nd number: "))
+num1 = int(input("1st number: "))
+num2 = int(input("2nd number: "))
+
+# get the numbers between 2 numbers
+if num1 < num2 :
+    for i in range (num1, num2) :
+        print(i)
