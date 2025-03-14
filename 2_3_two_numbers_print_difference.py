066fb0c7d59399a95ef0383a53d2cdde2f138536
allenandreim <allenandrei8209@gmail.com>
2025-03-14 13:53:21 +0800

    code_output_2_3

--- 2_3_two_numbers_print_difference.py
+++ 2_3_two_numbers_print_difference.py
@@ -1,3 +1,13 @@
 # get 2 numbers
 # subtract 2 numbers
 # print result
+
+# get 2 numbers
+num1 = float(input("1st number: "))
+num2 = float(input("2nd number: "))
+
+# subtract 2 numbers
+difference = num1 - num2
+
+# print result
+print(difference)
