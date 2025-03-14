9f600334925b474921ffabb8687139682dbc3a66
allenandreim <allenandrei8209@gmail.com>
2025-03-14 14:50:13 +0800

    code_output_2_7

--- 2_7_get_10_numbers_print_how_many_even.py
+++ 2_7_get_10_numbers_print_how_many_even.py
@@ -1,3 +1,43 @@
 # get 10 numbers
 # check if a number is even using % 2
 # print how many even
+
+# Initialize even count
+even_count = 0
+
+# Get 10 numbers
+num1 = int(input("1st number: "))
+num2 = int(input("2nd number: "))
+num3 = int(input("3rd number: "))
+num4 = int(input("4th number: "))
+num5 = int(input("5th number: "))
+num6 = int(input("6th number: "))
+num7 = int(input("7th number: "))
+num8 = int(input("8th number: "))
+num9 = int(input("9th number: "))
+num10 = int(input("10th number: "))   
+
+# check if a number is even and count
+if num1 % 2 == 0:
+    even_count += 1
+if num2 % 2 == 0:
+    even_count += 1
+if num3 % 2 == 0:
+   even_count += 1
+if num4 % 2 == 0:
+    even_count += 1
+if num5 % 2 == 0:
+    even_count += 1
+if num6 % 2 == 0:
+    even_count += 1
+if num7 % 2 == 0:
+    even_count += 1
+if num8 % 2 == 0:
+    even_count += 1
+if num9 % 2 == 0:
+    even_count += 1
+if num10 % 2 == 0:
+    even_count += 1
+
+# print result
+print("There are", even_count, "even numbers.")
