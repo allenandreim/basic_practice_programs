ed1a66bdbffa49541b1b4c1cfee45d580432da74
allenandreim <allenandrei8209@gmail.com>
2025-03-15 02:56:51 +0800

    code_output_4_2

--- 4_2_find_most_frequent_duplicate_until_invalid.py
+++ 4_2_find_most_frequent_duplicate_until_invalid.py
@@ -2,3 +2,23 @@
 # stop if invalid
 # print most frequent duplicate
 
+# get a nummber
+numbers = []
+
+while True:
+    try:
+        num = float(input("Enter a number (invalid to stop): "))
+        numbers.append(num)
+    except ValueError:
+        if not numbers:
+            print("No numbers entered.")
+            break
+        # count duplicates
+        duplicates = {n: numbers.count(n) for n in numbers if numbers.count(n) > 1}
+        if duplicates:
+            most_frequent = max(duplicates, key=duplicates.get)  # print most frequent duplicate
+            print("Most frequent duplicate:", most_frequent)
+        else:
+            print("No duplicates found.")
+        print("Invalid input. Exiting...")
+        break
