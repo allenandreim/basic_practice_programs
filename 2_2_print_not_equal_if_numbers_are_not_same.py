eb9006ec51ea782945f00346743779b38a31b732
allenandreim <allenandrei8209@gmail.com>
2025-03-14 13:39:46 +0800

    code_output_2_2

--- 2_2_print_not_equal_if_numbers_are_not_same.py
+++ 2_2_print_not_equal_if_numbers_are_not_same.py
@@ -1,3 +1,13 @@
 # get two numbers
 # check if numbers are not same
 # print not equal if numbers are not same
+
+# get two numbers
+num1 = float(input("1st number: "))
+num2 = float(input("2nd number: "))
+
+# check if numbers are not  same
+if num1 != num2 :
+    print("Not Equal")
+else :
+    print("Equal")
