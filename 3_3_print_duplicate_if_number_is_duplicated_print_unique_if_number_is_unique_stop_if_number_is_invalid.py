ba8c79d5dfda5647d6213908086d38a75c3368d0
allenandreim <allenandrei8209@gmail.com>
2025-03-14 22:35:10 +0800

    code_output_3_3

--- 3_3_print_duplicate_if_number_is_duplicated_print_unique_if_number_is_unique_stop_if_number_is_invalid.py
+++ 3_3_print_duplicate_if_number_is_duplicated_print_unique_if_number_is_unique_stop_if_number_is_invalid.py
@@ -2,3 +2,18 @@
 # print duplicate if number is duplicated, print unique if number is unique
 # stop program if input is invalid
 
+numbers = [] # initialize number
+
+# get numbers
+while True:
+    try :
+        num = float(input("type a number(input invalid to stop): "))
+        if num in numbers :
+            print("duplicate")
+
+        else :
+            print("unique")
+        numbers.append(num)
+    except ValueError :
+        print("invalid")
+        break
