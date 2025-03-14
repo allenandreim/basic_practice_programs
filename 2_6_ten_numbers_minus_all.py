0c8d0938f43e667a39e309436ad00f4f346a5082
allenandreim <allenandrei8209@gmail.com>
2025-03-14 14:37:55 +0800

    code_output_2_6

--- 2_6_ten_numbers_minus_all.py
+++ 2_6_ten_numbers_minus_all.py
@@ -2,5 +2,21 @@
 # subtract all numbers
 # print result
 
+# get 10 numbers
+num1 = float(input("1st number: "))
+num2 = float(input("2nd number: "))
+num3 = float(input("3rd number: "))
+num4 = float(input("4th number: "))
+num5 = float(input("5th number: "))
+num6 = float(input("6th number: "))
+num7 = float(input("7th number: "))
+num8 = float(input("8th number: "))
+num9 = float(input("9th number: "))
+num10 = float(input("10th number: "))
 
+# subtract all numbers
+difference = num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10
+
+# print result
+print("the difference of all number is:", difference)
 
