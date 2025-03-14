ad9b449ff77a39917da578a938b49269610e89db
allenandreim <allenandrei8209@gmail.com>
2025-03-14 13:33:27 +0800

    code_output_2_1

--- 2_1_two_numbers_print_smaller.py
+++ 2_1_two_numbers_print_smaller.py
@@ -1,3 +1,15 @@
 # get two numbers
 # check which is smaller
 # print result
+
+# get two numbers
+num1 = float(input("1st number: "))
+num2 = float(input("2nd number: "))
+ 
+# check which is smaller
+if num1 > num2 :
+    print(num2,"is smaller.")
+elif num2 > num1 :
+    print(num1,"is smaller.")
+else:
+    print("numbers are equal.")
