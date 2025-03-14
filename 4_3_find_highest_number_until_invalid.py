6101ee24dc491ba7d20dca9a5843b9f1e9267284
allenandreim <allenandrei8209@gmail.com>
2025-03-15 03:03:46 +0800

    code_output_4_3

--- 4_3_find_highest_number_until_invalid.py
+++ 4_3_find_highest_number_until_invalid.py
@@ -2,3 +2,17 @@
 # stop if invalid
 # print highest number
 
+# get a numbers
+numbers = []
+
+while True:
+    try:
+        num = float(input("Enter a number (invalid to stop): "))
+        numbers.append(num)
+    except ValueError:   # stop if invalid
+        if numbers:
+            print("highest number:", max(numbers))  # print highest number
+        else:
+            print("no numbers entered.")
+        print("invalid")
+        break
