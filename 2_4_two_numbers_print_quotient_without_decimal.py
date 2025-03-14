e83336962c43745e9b0ba218b6d08715f7784ebb
allenandreim <allenandrei8209@gmail.com>
2025-03-14 14:12:07 +0800

    code_output_2_4

--- 2_4_two_numbers_print_quotient_without_decimal.py
+++ 2_4_two_numbers_print_quotient_without_decimal.py
@@ -2,3 +2,15 @@
 # divide num1 to num2
 # restrict 0 in divisor 
 # print result (no decimal)
+
+# get 2 numbers
+num1 = float(input("1st number(dividend): "))
+num2 = float(input("2nd number(divisor): "))
+
+
+# restrict 0 in divisor 
+if num2 != 0 :
+    quotient = int(num1 // num2)  # divide num1 to num2
+    print("quotient (without decimal):", quotient)
+else :
+    print("undefined") 
