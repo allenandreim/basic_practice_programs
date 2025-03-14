e702c5998432608cc62bd9ce89692efc7a4070f5
allenandreim <allenandrei8209@gmail.com>
2025-03-14 15:33:51 +0800

    code_output_2_9

---
+++ 2_9_print_numbers_except_ending_5_or_0.py
@@ -0,0 +1,8 @@
+# get range from 0 to 100
+# check if number ending is 0  and 5
+# print result
+
+# get range from 0 to 100
+for i in range (101) :
+    if i % 5 != 0 :  # this check if number ending is 0 and 5 by using % 5
+        print(i)  # print result
