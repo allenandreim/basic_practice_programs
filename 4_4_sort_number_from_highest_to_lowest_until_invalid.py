74e117ef745bb9faf40465628f6687018b2c72ae
allenandreim <allenandrei8209@gmail.com>
2025-03-15 03:11:01 +0800

    pseudocode_4_4

--- 4_4_sort_number_from_highest_to_lowest_until_invalid.py
+++ 4_4_sort_number_from_highest_to_lowest_until_invalid.py
@@ -2,3 +2,17 @@
 # stop if invalid
 # sort numbers descending order
 
+# get a number
+numbers = []
+while True :
+    try :
+        num = float(input("type a number (input invalid to stop): "))
+        numbers.append(num)
+    except ValueError :
+        if numbers :
+            numbers.sort(reverse=True)  # sort numbers in descending order
+            print(numbers)  # print sorted numbers
+        else :
+            print("no numbers entered")
+        print("invalid")  # stop if invalid
+        break
