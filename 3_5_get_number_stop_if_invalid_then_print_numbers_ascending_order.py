d920b48bb456512a4b8e0ebd3c8bbf7fec6bc388
allenandreim <allenandrei8209@gmail.com>
2025-03-15 01:49:15 +0800

    code_output_3_5

--- 3_5_get_number_stop_if_invalid_then_print_numbers_ascending_order.py
+++ 3_5_get_number_stop_if_invalid_then_print_numbers_ascending_order.py
@@ -2,3 +2,17 @@
 # stop if invalid
 # print numbers in ascending order
 
+# get a number
+numbers = []
+while True :
+    try :
+        num = float(input("type a number (input invalid to stop): "))
+        numbers.append(num)
+    except ValueError :
+        if numbers :
+            numbers.sort()  # sort numbers in ascending order
+            print(numbers)  # print sorted numbers
+        else :
+            print("no numbers entered")
+        print("invalid")  # stop if invalid
+        break
