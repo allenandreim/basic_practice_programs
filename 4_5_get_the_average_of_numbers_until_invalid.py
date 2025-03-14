3d3f73544c87e1990fd3c0e156ada73f6fe535e3
allenandreim <allenandrei8209@gmail.com>
2025-03-15 03:40:23 +0800

    code_output_4_5''

--- 4_5_get_the_average_of_numbers_until_invalid.py
+++ 4_5_get_the_average_of_numbers_until_invalid.py
@@ -2,3 +2,18 @@
 # stop if invalid
 # get the average of number
 
+# get a number
+numbers = []
+
+while True:
+    try:
+        num = float(input("Enter a number (invalid to stop): "))
+        numbers.append(num)
+    except ValueError:  # stop if invalid
+        if numbers:
+            average = sum(numbers) / len(numbers)  # get the average of numbers
+            print("average:", average)
+        else:
+            print("no numbers entered.")
+        print("invalid")
+        break
