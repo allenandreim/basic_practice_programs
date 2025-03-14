6fe7fda367c6ff9b979aa81f542497f5e4c02f73
allenandreim <allenandrei8209@gmail.com>
2025-03-14 15:04:01 +0800

    code_output_2_8

--- 2_8_print_all_odds_from_0_to_100.py
+++ 2_8_print_all_odds_from_0_to_100.py
@@ -1,3 +1,7 @@
 # set range 0 to 100
-# check if number is even
+# check if number is odd
 # print result
+
+# set range 0 to 100
+for i in range (1,101,2) :  # (1,101,2) this range starts the count into 1 then has a gap of 2 to print all odd numbers
+    print(i)   # print result
