9e9541540f0c5e72703df147a74fa872fed1b340
allenandreim <allenandrei8209@gmail.com>
2025-03-14 22:57:35 +0800

    code_output_3_4

--- 3_4_get_number_stop_if_invalid_then_print_lowest.py
+++ 3_4_get_number_stop_if_invalid_then_print_lowest.py
@@ -2,3 +2,18 @@
 # stop if invalid
 # print smallest
 
+# get a number
+numbers = []
+
+while True :
+    try:
+        num = float(input("type a number(input invalid to stop): "))
+        numbers.append(num)
+    except ValueError :
+        if numbers :     
+            print("lowest number:", min(numbers))
+        else :
+            print("no numbers entered")
+        print("invalid")            
+        break
+    
