954a0d77eac638fb2e5f2e2ac6775f7e9c5006ea
allenandreim <allenandrei8209@gmail.com>
2025-03-14 18:07:20 +0800

    code_outpu_3_1

--- 3_1_input_10_numbers_print_unique_numbers.py
+++ 3_1_input_10_numbers_print_unique_numbers.py
@@ -2,4 +2,26 @@
 # find unique numbers
 # print result
 
-print("a")
+# get 10 numbers
+num1 = float(input("1st number: "))
+num2 = float(input("2nd number: "))
+num3 = float(input("3rd number: "))
+num4 = float(input("4th number: "))
+num5 = float(input("5th number: "))
+num6 = float(input("6th number: "))
+num7 = float(input("7th number: "))
+num8 = float(input("8th number: "))
+num9 = float(input("9th number: "))
+num10 = float(input("10th number: "))
+
+numbers = [num1, num2, num3, num4, num5, num6, num7, num8, num9, num10]
+
+# find unique numbers
+unique_numbers = [num for num in numbers if numbers.count(num) == 1]
+
+# print result
+if unique_numbers:
+    print("Unique numbers:", unique_numbers)
+else:
+    print("No unique numbers found.")
+
