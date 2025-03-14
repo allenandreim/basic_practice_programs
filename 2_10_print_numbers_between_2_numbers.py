7c2f46e3625680cae9e4cf52a1570105639f5d1d
allenandreim <allenandrei8209@gmail.com>
2025-03-14 15:51:39 +0800

    code_output_2_10

--- 2_10_print_numbers_between_2_numbers.py
+++ 2_10_print_numbers_between_2_numbers.py
@@ -8,6 +8,13 @@ num1 = int(input("1st number: "))
 num2 = int(input("2nd number: "))
 
 # get the numbers between 2 numbers
-if num1 < num2 :
-    for i in range (num1, num2) :
+if num1 < num2 : 
+    for i in range (num1 + 1, num2) :
         print(i)
+
+elif num1 > num2 :
+    for i in range (num2 + 1, num1) :
+        print (i)
+
+else : 
+    print("numbers are equal and has no other numbers in between.")
