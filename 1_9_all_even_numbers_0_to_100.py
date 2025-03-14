666a17eda8f9b729947fcfc00d3fe4ecd752db87
allenandreim <allenandrei8209@gmail.com>
2025-03-14 13:18:42 +0800

    code_output_1_9

--- 1_9_all_even_numbers_0_to_100.py
+++ 1_9_all_even_numbers_0_to_100.py
@@ -1,3 +1,8 @@
 # set range 0 to 100
 # check if number is even
 # print result
+
+# set range 0 to 100
+for i in range (0,101,2) :  # (0,101,2) this range starts the count into 2 then print all even numbers
+    print(i)   # print result
+
