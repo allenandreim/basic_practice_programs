1c5e5fd1ea4af164937143f23c0174516e5082b3
allenandreim <allenandrei8209@gmail.com>
2025-03-14 14:24:07 +0800

    code_output_2_5

--- 2_5_divide_two_numbers_print_remainder.py
+++ 2_5_divide_two_numbers_print_remainder.py
@@ -2,3 +2,15 @@
 # divide 2 numbers
 # print remainder
 
+# get 2 numbers
+num1 = float(input("1st number(dividend): "))
+num2 = float(input("2nd number(divisor): "))
+
+# divide num1 to num2 (use % to get remainder)
+remainder = num1 % num2
+
+# restrict 0 in divisor 
+if num2 != 0 :
+    print("the remainder is:", remainder)
+else :
+    print("undefined")
