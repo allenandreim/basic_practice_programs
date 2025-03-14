07cc72db3c3ebf96f2cc25c3a16d2816c1487aaf
allenandreim <allenandrei8209@gmail.com>
2025-03-14 13:27:02 +0800

    code_output_1_10

--- 1_10_print_numbers_except_ending_0.py
+++ 1_10_print_numbers_except_ending_0.py
@@ -2,3 +2,7 @@
 # check if number ending is 0
 # print result
 
+# get range from 0 to 100
+for i in range (101) :
+    if i % 10 != 0 :  # this check if number ending is 0 by using % 10
+        print(i)  # print result
