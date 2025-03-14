d44d9473c8014b7690c75d48fbbdcfec4b7ec101
allenandreim <allenandrei8209@gmail.com>
2025-03-14 15:41:20 +0800

    initial_commit_input_2_numbers

--- 2_10_print_numbers_between_2_numbers.py
+++ 2_10_print_numbers_between_2_numbers.py
@@ -1,3 +1,8 @@
 # get 2 numbers
 # get the numbers between 2 numbers
 # print result
+
+# get 2 numbers
+print("input two numbers (whole numbers only)")
+num1 = float(input("1st number: "))
+num2 = float(input("2nd number: "))
