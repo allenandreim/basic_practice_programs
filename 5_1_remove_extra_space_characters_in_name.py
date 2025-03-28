2ac0ede4afd6d6f9c9cfb1dc4fe3b92711fc6c57
allenandreim <allenandrei8209@gmail.com>
2025-03-28 21:01:20 +0800

    code_output_5_1'

--- 5_1_remove_extra_space_characters_in_name.py
+++ 5_1_remove_extra_space_characters_in_name.py
@@ -2,3 +2,11 @@
 # remove extra space
 # print output
 
+# input full name with extra space
+name = input("full name with extrea spaces: ")
+
+# remove extra space
+fixed_name = name.lstrip()
+
+# print output
+print("fullname:", fixed_name)  
