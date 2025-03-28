f235f0aad6fc1585e3a0671125cee9e9b4e52ce0
allenandreim <allenandrei8209@gmail.com>
2025-03-28 21:18:59 +0800

    code_output_5_2

--- 5_2_format_number_form_0_to_1000.py
+++ 5_2_format_number_form_0_to_1000.py
@@ -1,7 +1,14 @@
-# get number rom 0-1000
-# add leadind zeroes to make it 6 digit format
+# get number from 0-1000
+# add leading zeroes to make it 6 digit format
 # print output 
 
+# get number from 0-1000
+num = int(input("type number(0-1000): "))
 
+# add leading zeroes to make it 6 digit format
+format_num = f"{num:06d}"
+
+# print output 
+print(format_num)
 
 
