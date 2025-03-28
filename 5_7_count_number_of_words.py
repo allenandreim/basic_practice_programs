a464e882fe28ae5fc20741ff1ac14b6a6b8df0ad
allenandreim <allenandrei8209@gmail.com>
2025-03-28 23:23:34 +0800

    code_output_5_7

--- 5_7_count_number_of_words.py
+++ 5_7_count_number_of_words.py
@@ -2,3 +2,11 @@
 # count every words
 # print output
 
+# type a sentence
+sentence = input("type a sentence: ")
+
+# count every words
+word_count = len(sentence.split())
+
+# print output
+print("word count:" , word_count)
