c304209f2d53ab0ed4379a9711a103860aa11842
allenandreim <allenandrei8209@gmail.com>
2025-03-29 00:46:37 +0800

    code_output_5_10

--- 5_10_make_name_snake_case.py
+++ 5_10_make_name_snake_case.py
@@ -2,5 +2,12 @@
 # make it in snake case
 # print output 
 
+# input full name
+name = input("full name: ")
 
+# make it in snake case
+words = name.split()
+sncase_name = "_".join(word.lower() for word in words)
 
+# print output 
+print(sncase_name)
